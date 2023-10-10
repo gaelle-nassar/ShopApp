265d24674df7a724c164adcb6630cb52575f23d2
Gaelle Nassar <201820543@ua.edu.lb>
2023-10-10 14:47:05 +0300

    add cart

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,7 @@ import { useFonts } from 'expo-font';
 import { SplashScreen, Stack } from 'expo-router';
 import { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
-
+import { CartProvider, useCart } from '../app/CartContext';
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
@@ -46,11 +46,13 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
+    <CartProvider>
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
       </Stack>
     </ThemeProvider>
+    </CartProvider>
   );
 }
